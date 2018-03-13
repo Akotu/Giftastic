a5c9cc10d036b3f8977b236bcbd763e482969e7d
Maverick <embrymaverick@gmail.com>
2018-03-13 12:35:43 -0700

    PseudoCode

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -4,15 +4,12 @@ $(document).ready(function () {
   var topics = ["Runescape", "Overwatch", "Call of duty", "Fallout", "Halo 3", "Borderlands", "League of Legends",
     "Fortnite", "PubG"
   ];
-
+  //use a function to create a list of buttons based off topics array
   function createButtons() {
     $(".buttons").empty();
     for (var i = 0; i < topics.length; i++) {
 
-
-
       var button = $("<button class='btn btn-outline-secondary topic' type='button'></button>");
-
 
       button.attr("value", topics[i]);
 
@@ -20,13 +17,14 @@ $(document).ready(function () {
 
       $(".buttons").append(button);
 
-
     };
   };
 
 
-
+  // create a function to display the gifs animated, and still
   function gify(response, value) {
+
+    console.log(response);
 
     $(".gifs").empty();
 
@@ -34,12 +32,14 @@ $(document).ready(function () {
 
     for (var i = 0; i < Count; i++) {
 
+      //find the JSON objects
       var still = response.data[i].images.fixed_height_small_still.url;
 
       var animate = response.data[i].images.fixed_height_small.url;
-
+      
       var img = $("<img src='" + still + "'>");
 
+      //add attributes to the image so we can alter the image's state
       img.attr("alt", value);
 
       img.attr("data-still", still);
@@ -51,10 +51,13 @@ $(document).ready(function () {
       img.attr("class", "gifImg");
 
       var p = $("<p>");
+      //rating of the gif
 
       p.text("Rating: " + response.data[i].rating);
 
       var div = $("<div>");
+
+      //show 10 instead of 9 gifs..
 
       div.attr("id", "img" + (i + 1));
 
@@ -66,13 +69,15 @@ $(document).ready(function () {
     }
   };
 
-
+  //display the buttons on start
   createButtons();
 
   $(document).on("click", ".topic", function (event) {
     console.log($(this).attr("value"));
 
     var value = $(this).attr("value");
+
+    //i found this method of grabbing an API, i thought it was less code IMO instead of $ajax
 
     $.get(
       "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + value +
@@ -84,6 +89,8 @@ $(document).ready(function () {
       gify(response, value);
     });
   });
+
+  //add your own animal button
 
   $(".submit").on("click", function (event) {
 
@@ -97,6 +104,7 @@ $(document).ready(function () {
     createButtons();
   });
 
+  //when you click on the gif, play it, click it again, pause it
   $(document).on("click", ".gifImg", function (e) {
 
     var state = $(this).attr("data-state");
